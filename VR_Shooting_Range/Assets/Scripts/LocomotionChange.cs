c5261642764d41823a821a8b645dfb77b973798b
Daniil <daniildu437@gmail.com>
2024-11-24 14:44:52 +0100

    Add simple objects

---
+++ VR_Shooting_Range/Assets/Scripts/LocomotionChange.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocomotionChange : MonoBehaviour
+{
+    public ContinuousMoveProviderBase moveProvider;
+    public Transform cameraTransform;
+    public Transform controllerTransform; 
+    public Button cameraButton; 
+    public Button controllerButton; 
+
+    void Start()
+    {
+        cameraButton.onClick.AddListener(CameraSwitch);
+        controllerButton.onClick.AddListener(ControllerSwitch);
+
+        moveProvider.forwardSource = cameraTransform;
+    }
+
+    void CameraSwitch()
+    {
+        moveProvider.forwardSource = cameraTransform;
+        Debug.Log("Camera forward source selected.");
+    }
+
+    void ControllerSwitch()
+    {
+        moveProvider.forwardSource = controllerTransform;
+        Debug.Log("Controller forward source selected.");
+    }
+}
