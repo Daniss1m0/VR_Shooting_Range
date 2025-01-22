7efcb5c1794c586d7e52cb68fc345be8557d7ff0
Daniil <daniildu437@gmail.com>
2025-01-22 18:59:26 +0100

    Version 1.0

--- VR_Shooting_Range/Assets/Scripts/HitCounter.cs
+++ VR_Shooting_Range/Assets/Scripts/HitCounter.cs
@@ -1,8 +1,24 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HitCounter : MonoBehaviour
 {
     public static int TotalHits { get; private set; } = 0;
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        TotalHits = 0;
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
